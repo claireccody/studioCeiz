2478f8fc6244c5225196ef46c6733f22413f4a0b
claireccody <ccody_uo@hotmail.com>
2026-02-08 00:59:49 -0800

    bug fixing

--- dyamics.js
+++ dyamics.js
@@ -1,11 +1,30 @@
-function modalToggle(){
-    window.scrollTo(0,0);
-    console.log('toggle?')
-    document.querySelectorAll(".sect").forEach(element => {
-        element.classList.toggle("hide");
-    });
-    document.getElementById("form").classList.toggle("hide");
-    document.getElementById("footer").classList.toggle("hide");
-    document.getElementById("top").classList.toggle("low");
-    document.getElementById("defaultCanvas0").classList.toggle("low");
+function modalToggle() {
+  window.scrollTo(0, 0);
+  console.log("toggle?");
+  document.querySelectorAll(".sect").forEach((element) => {
+    element.classList.toggle("hide");
+  });
+  document.getElementById("form").classList.toggle("hide");
+//   document.getElementById("footer").style.display="low";
+document.getElementById("footer").classList.toggle("low");
+document.getElementById("top").classList.toggle("low");
+
+    document.querySelectorAll(".sect").forEach((element) => {
+    element.classList.toggle("seek");
+  });
+  document.getElementById("form").classList.toggle("seek");
+//   document.getElementById("footer").classList.toggle("seek");
+
+//   document.getElementById("top").classList.toggle("low");
+  // document.getElementById("defaultCanvas0").classList.toggle("low");
 }
+
+document.getElementById("openFormB").addEventListener("click", () => {
+  modalToggle();
+});
+document.getElementById("openFormA").addEventListener("click", () => {
+  modalToggle();
+});
+document.getElementById("openFormC").addEventListener("click", () => {
+  modalToggle();
+});
