40ed2d7b5080ef64c7068adc78479aadafc1fd23
claireccody <ccody_uo@hotmail.com>
2026-02-08 00:08:07 -0800

    aesthetic tweaks

--- dyamics.js
+++ dyamics.js
@@ -1,7 +1,11 @@
 function modalToggle(){
+    window.scrollTo(0,0);
     console.log('toggle?')
     document.querySelectorAll(".sect").forEach(element => {
         element.classList.toggle("hide");
     });
     document.getElementById("form").classList.toggle("hide");
+    document.getElementById("footer").classList.toggle("hide");
+    document.getElementById("top").classList.toggle("low");
+    document.getElementById("defaultCanvas0").classList.toggle("low");
 }
